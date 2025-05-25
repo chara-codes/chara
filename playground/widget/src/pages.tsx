422e10b32b94fad992fb9018629fabe50fff3b16
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2025-05-25 08:51:57 +0200

    remove widget from playground

--- playground/widget/src/pages.tsx
+++
@@ -1,9 +0,0 @@
-import ChatOverlayPanel from "./components/templates/chat-overlay-panel";
-
-export default function Page() {
-  return (
-    <div className="min-h-screen bg-background">
-      <ChatOverlayPanel />
-    </div>
-  );
-}
