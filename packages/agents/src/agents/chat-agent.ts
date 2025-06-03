cbdf067b3a738be11e412b9daecaea5cf8a79191
Andrey Kucherenko <kucherenko.andrey@gmail.com>
2025-06-03 19:35:47 +0200

    Add weather tool and form element tests

--- packages/agents/src/agents/chat-agent.ts
+++ packages/agents/src/agents/chat-agent.ts
@@ -1,6 +1,7 @@
-import { streamText, type CoreMessage } from "ai";
+import { StreamData, streamText, tool, type CoreMessage } from "ai";
 import { providersRegistry } from "../providers";
 import { logger } from "@chara/logger";
+import z from "zod";
 
 export const chatAgent = (
   {
@@ -16,11 +17,27 @@ export const chatAgent = (
     model.split(":::");
   const aiModel = providersRegistry.getModel(providerName, modelName);
   logger.info(providerName, modelName);
+
+  // const data = new StreamData();
+
   return streamText({
     ...options,
     system: "You are a helpful assistant.",
+    tools: {
+      weather: tool({
+        description: "Get the weather in a location",
+        parameters: z.object({
+          location: z.string().describe("The location to get the weather for"),
+        }),
+        execute: async ({ location }) => ({
+          location,
+          temperature: 72 + Math.floor(Math.random() * 21) - 10,
+        }),
+      }),
+    },
     model: aiModel,
     toolCallStreaming: true,
+    maxSteps: 10,
     messages: [{ role: "system", content: "Be funny" }, ...messages],
   });
 };
